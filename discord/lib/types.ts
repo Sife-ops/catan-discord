bbc45e5136a182fefa3ccc7bbc749108284b9385
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-12-28 00:19:44 -0500

    types

---
+++ discord/lib/types.ts
@@ -0,0 +1,42 @@
+type Resource = "Gold" | "Spice" | "Wood" | "Lizard"
+type Colors = "White" | "Green" | "Black" | "Yellow"
+
+type Board = {
+  tiles: TileGraph,
+  roads: RoadGraph,
+}
+
+type RoadGraph = {
+  vertices: RoadVertex[],
+  edges: RoadEdge[],
+}
+
+/**
+ * The spot where a player can build a town or city
+ */
+type RoadVertex = {
+  building: undefined | "Town" | "City",
+  color: Colors | undefined,
+  adjacentTiles: [TileNode | undefined, TileNode | undefined, TileNode | undefined]
+}
+
+type RoadEdge = {
+  road: boolean,
+  color: Colors | undefined
+}
+
+type TileGraph = {
+  tiles: TileNode[],
+  edges: TileEdge[],
+}
+
+type TileNode = {
+  id: string,
+  resource: Resource,
+  roll: number,
+  robber: boolean,
+}
+
+type TileEdge = {
+  from: TileNode, to: TileNode
+}
