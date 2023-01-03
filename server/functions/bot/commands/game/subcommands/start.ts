2e110993b339f5465e65167a50d0f63a500ca971
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-01-02 21:33:11 -0800

    init game

--- server/functions/bot/commands/game/subcommands/start.ts
+++ server/functions/bot/commands/game/subcommands/start.ts
@@ -5,6 +5,13 @@ import { model } from "@catan-discord/core/model";
 export const start: Command = {
   schema: undefined,
   handler: async (_, { game, channelId }) => {
+    /**
+     * ) count players
+     * ) initialize game
+     * ) start game
+     */
+
+    // ) count players
     if (!game) throw new Error("missing game");
     const playerCount = await model.entities.PlayerEntity.query
       .game_({ gameId: game.gameId })
@@ -16,8 +23,58 @@ export const start: Command = {
     }
 
     // todo: "too many players"
-    // todo: initialize game
 
+    // ) initialize game
+    const map = JSON.parse(game.map) as { type: string }[][];
+    const mapCoords = map.map((row, iRow) =>
+      row.map((col, iCol) => ({
+        ...col,
+        coords: {
+          x: iCol,
+          y: iRow,
+        },
+      }))
+    );
+    const mapCoordsFlat = mapCoords.reduce((a, c) => {
+      return [...a, ...c];
+    }, []);
+
+    const terrains = mapCoordsFlat.filter((e) => e.type === "terrain");
+    const harbors = mapCoordsFlat.filter((e) => e.type === "harbor");
+
+    // todo: terrain, chit, harbor have a specific ratio based on board size
+
+    await Promise.all([
+      ...terrains.map(({ coords: { x, y } }) => {
+        model.entities.TerrainEntity.create({
+          gameId: game.gameId,
+          terrain: ["pasture", "fields", "mountains", "hills", "forest"][
+            Math.floor(Math.random() * 5)
+          ] as "pasture" | "fields" | "mountains" | "hills" | "forest",
+          x,
+          y,
+        }).go();
+      }),
+      ...terrains.map(({ coords: { x, y } }) => {
+        model.entities.ChitEntity.create({
+          gameId: game.gameId,
+          value: 1,
+          x,
+          y,
+        }).go();
+      }),
+      ...harbors.map(({ coords: { x, y } }) => {
+        model.entities.HarborEntity.create({
+          gameId: game.gameId,
+          resource: "brick",
+          ratio: "2:1",
+          x,
+          y,
+        }).go();
+      }),
+    ]);
+
+    // ) start game
     await model.entities.GameEntity.update({
       channelId,
       gameId: game.gameId,
