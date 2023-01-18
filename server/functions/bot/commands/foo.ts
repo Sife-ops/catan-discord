cb62612d4dc5497158a40d2ebbe668fd88818f92
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-01-17 16:27:57 -0800

    mvp

--- server/functions/bot/commands/foo.ts
+++ server/functions/bot/commands/foo.ts
@@ -1,8 +1,48 @@
-import { genericResponse } from "@catan-discord/bot/common";
+import AWS from "aws-sdk";
 import { Command } from "../runner";
+import { genericResponse } from "@catan-discord/bot/common";
+import { model } from "@catan-discord/core/model";
+
+const sendMessageToClient = (url: string, connectionId: string, payload: any) =>
+  new Promise((resolve, reject) => {
+    const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
+      // apiVersion: "2018-11-29",
+      endpoint: url,
+    });
+
+    apigatewaymanagementapi.postToConnection(
+      {
+        ConnectionId: connectionId, // connectionId of the receiving ws-client
+        Data: JSON.stringify(payload),
+      },
+
+      (err, data) => {
+        if (err) {
+          console.log("err is", err);
+          reject(err);
+        }
+
+        resolve(data);
+      }
+    );
+  });
 
 export const foo: Command = {
   handler: async () => {
+    const connectionIds = await model.entities.ConnectionEntity.scan
+      .go()
+      .then((e) => e.data.map((e) => e.connectionId));
+
+    await Promise.all(
+      connectionIds.map((connectionId) =>
+        sendMessageToClient(
+          "15hiut6zyb.execute-api.us-east-1.amazonaws.com/wgoettsch",
+          connectionId,
+          { message: "test test test" }
+        )
+      )
+    );
+
     return genericResponse("bar");
   },
 };
